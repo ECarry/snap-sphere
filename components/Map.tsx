fd6ed9cf0a4acdfd1953868659ceb83bcd3c5d0b
ecarry <lianshiliang93@gmail.com>
2025-01-25 00:09:15 +0800

    docs: env file

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,12 @@
 import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
 
-const accessToken =
-  'pk.eyJ1IjoiZWNhcnJ5IiwiYSI6ImNsbDR4enltcDA4Z2EzbG8xb2ozOXA0bmoifQ.wnF8fme0DPl9Fah7tFjGdA';
-Mapbox.setAccessToken(accessToken);
+Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN || '');
 
 const Map = () => {
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/satellite-v9">
       <Camera followZoomLevel={16} followUserLocation />
-      <LocationPuck />
+      <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
     </MapView>
   );
 };
